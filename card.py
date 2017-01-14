3ca4cdde9d79cbbcab6c99e794e91d9d06a336b7
cvraut <chinmay0raut@gmail.com>
2017-01-14 00:47:51 -0800

    finished the dependancies for card

--- card.py
+++ card.py
@@ -1,4 +1,5 @@
 class card:
-    def __init__(self,group='',name='',price=0,):
+    def __init__(self,group='',name='',rent=0,one_house=0,two_house=0,three_house=0,four_house,hotel=0,mortgage_value=0,house_cost=0,hotel_cost=0):
         self._group = group
-        self._
+        self._name = name
+        self._rent = rent
