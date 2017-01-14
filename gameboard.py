2ce4fd052f5518c07d4ec1f3cbce497a072e4dbe
Mithil Hari <mithilhari@yahoo.com>
2017-01-14 03:19:12 -0800

    nothing

--- gameboard.py
+++ gameboard.py
@@ -1,39 +1,32 @@
 
 from card import base_card
 from card import property_card
+from card import special_card
 
 class gameboard:
     def __init__(self):
-        self.board = [] # This board is an array of all the cards
-        self.open_file()
+        self._board = [] # This board is an array of all the cards
+        self._file = open('Property.txt', 'r')
         self.iterate_file()
         
-    def open_file (self):
-        self.file = open ("Property.txt", "r") # opens the readable file
-    
-    def iterate_file (self):
-        for i in range (0, 40):
-            self.read_string () # processes the string
 
-    def read_string (self):
-        arr = self.file.readline().replace("\n","").split(" ") # split into different parts
+    def iterate_file(self):
+        for line in self.file:
+            self.read_string(line) # processes the string
+
+
+    def read_string(self, line):
+        arr = line.rstrip().split(' ') # split into different parts
         if (arr[0] == "Special"):
-            newcard = (arr[1])
+            newcard = special_card(arr[1])
         elif (arr[0] == "Property"):
-            newcard = self.new_property_card(arr)
+            newcard = property_card(arr[2], arr[1], int(arr[9]), arr[3:9], 0, 0, int(arr[9])/2, int(arr[10]))
         elif (arr[0] == "Railroad"):
-            newcard = self.new_railroad_card(arr)
+            newcard = base_card("", arr[1], 200, [25,50,100,200], 0)
         elif (arr[0] == "Utility"):
-            newcard = self.new_utility_card(arr)
-        self.board.append(newcard)
-        #print (newcard)
-
-    def new_property_card(self, arr):
-        return arr[1]
-    def new_railroad_card(self, arr):
-        return arr[1]
-    def new_utility_card(self, arr):
-        return arr[1]
+            newcard = base_card("", arr[1], 150, [4,10], 0)
+        self._board.append(newcard)
+        print(newcard.get_name())
 
 if __name__ == '__main__':
     obj = gameboard()
