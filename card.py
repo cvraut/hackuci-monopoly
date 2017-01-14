fcd14a39d783f3c670aebc150957aff203da270d
lawrencekhlim <lawrencekhlim@gmail.com>
2017-01-14 02:59:36 -0800

    Add files via upload

--- card.py
+++ card.py
@@ -1,44 +1,40 @@
 class base_card:
-    def __init__(self,group='',name='',cost=0,rent=0,house_rents=[],num_houses=0):
-        self._group = group
+    def __init__(self,name='',cost=0,house_rents=[],num_houses=0):
+        #self._group = group
         self._cost = cost
         self._name = name
-        self._rent = rent
         self._house_costs = house_rents
         self._num_houses = num_houses
 
     def get_group(self):
-         return self._group
+        return self._group
     def get_cost(self):
         return self._cost
     def get_name(self):
         return self._name
-    def get_rent(self):
-        return self._rent
     def get_total_rent(self):
-        return self._house_rents[self._num_houses]
+        return self._house_costs[self._num_houses]
     def get_num_houses(self):
         return self._num_houses
     def get_house_rents(self):
-        return self._house_rents
+        return self._house_costs
 
 
 
 class property_card(base_card):
-    def __init__(self,group='',name='',cost=0,rent=0,house_rents=[],num_houses=0,hotel=0,mortgage_value=0,house_cost=0,hotel_cost=0):
-        super().__init__(group,name,cost,rent,house_rents,num_houses)
+    def __init__(self,group='',name='',cost=0,house_rents=[],num_houses=0,hotel=0,mortgage_value=0,house_cost=0):
+        super().__init__(name,cost,house_rents,num_houses)
+        self._group = group
         self._hotel = hotel
         self._mortgage_value = mortgage_value
         self._house_cost = house_cost
-        self._hotel_cost = hotel_cost
     def get_hotel(self):
         return self._hotel
     def get_mortgage_value(self):
         return self._mortgage_value
     def get_house_cost(self):
         return self._house_cost
-    def get_hotel_cost(self):
-        return self._hotel_cost
+
 
 
 class special_card(base_card):
--- gameboard.py
+++ gameboard.py
@@ -20,14 +20,24 @@ class gameboard:
         arr = self.file.readline().rstrip().split(" ") # split into different parts
         if (arr[0] == "Special"):
             newcard = special_card (arr[1])
+            print (newcard.get_name())
         elif (arr[0] == "Property"):
             newcard = property_card(arr[2], arr[1], int(arr[9]), arr[3:9], 0, 0, int(arr[9])/2, int(arr[10]))
+            print (newcard.get_name())
+            print (newcard.get_hotel())
+            print (newcard.get_mortgage_value())
+            print(newcard.get_total_rent())
+            
         elif (arr[0] == "Railroad"):
-            newcard = base_card("", arr[1], 200, [25,50,100,200], 0)
+            newcard = base_card(arr[1], 200, [25,50,100,200], 0)
+            print(newcard.get_name())
+            print(newcard.get_total_rent())
         elif (arr[0] == "Utility"):
-            newcard = base_card("", arr[1], 150, [4,10], 0)
+            newcard = base_card(arr[1], 150, [4,10], 0)
+            print(newcard.get_name())
+            print(newcard.get_total_rent())
         self.board.append(newcard)
-        print(newcard.get_name())
+        #print(newcard.get_name())
         
 
 if __name__ == '__main__':
