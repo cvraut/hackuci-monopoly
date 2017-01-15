bbbe526f2cdf0a176d25c1a0c2b504c93be1951e
Mithil Hari <mithilhari@yahoo.com>
2017-01-15 10:41:41 -0800

    incomplete test file

---
+++ ga_worker_test.py
@@ -0,0 +1,48 @@
+"""
+Property Mediterranean_Avenue Purple
+Property Baltic_Avenue Purple
+
+
+Railroad Reading_Railroad
+Railroad Pennsylvania_Railroad
+Railroad B.&O._Railroad
+Railroad Short_Line
+
+
+Property Oriental_Avenue Light_Blue
+Property Vermont_Avenue Light_Blue
+Property Connecticut_Avenue Light_Blue
+
+
+Property St._Charles_Place Light_Purple
+Property States_Avenue Light_Purple
+Property Virginia_Avenue Light_Purple
+
+
+Property St._James_Place Orange
+Property Tennessee_Avenue Orange
+Property New_York_Avenue Orange
+
+
+Property Kentucky_Avenue Red
+Property Indiana_Avenue Red
+Property Illinois_Avenue Red
+
+
+Property Atlantic_Avenue Yellow
+Property Ventnor_Avenue Yellow
+Property Marvin_Gardens Yellow
+
+
+Utility Electric_Company
+Utility Water_Works
+
+
+Property Pacific_Avenue Green
+Property North_Carolina_Avenue Green
+Property Pennsylvania_Avenue Green
+
+
+Property Park_Place Blue
+Property Boardwalk Blue
+"""
